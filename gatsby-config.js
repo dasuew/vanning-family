088d8f94e6133f81493c4ef03d52b209d6359e64
Uwe Manteuffel <dasuew@hotmail.de>
2020-05-06 11:33:18 +0200

    add gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  plugins: [
+  pathPrefix: "/vanning-family",
+  plugins: [    
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
