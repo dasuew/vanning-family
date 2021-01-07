a3df4109ac5792097dbc9e505813d16f2e8a4385
Uwe Manteuffel <uwe.manteuffel@zeiss.com>
2021-01-07 23:41:50 +0100

    update package

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
-module.exports = () => ({
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        require('postcss-preset-env')({
-            preserve: false,
-            browsers: 'last 2 versions',
-            stage: 3,
-            features: {
-                'nesting-rules': true,
-            },
-        })
-    ]
-})
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      'postcss-preset-env': {
+        preserve: false,
+        browsers: 'last 2 versions',
+        stage: 3,
+        features: {
+            'nesting-rules': true,
+        }
+      }
+    }
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
